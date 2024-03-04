b4d7bf0cbcb220037b961ebb573b4ca3adbc5342
1119879311 <1119879311@qq.com>
2024-03-04 22:02:37 +0800

    feat: 资源表重新设计

--- src/entity/tk_resource.entity.ts
+++ src/entity/tk_resource.entity.ts
@@ -1,9 +1,15 @@
+/**
+ * 资源表
+ */
+
+
+
 import { tk_role } from './tk_role.entity';
 import { Column, CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { tk_common } from './tk_common';
 
-@Entity("tk_authority")
-export class tk_authority extends tk_common{
+@Entity("tk_resource")
+export class tk_resource extends tk_common{
   
 
     @Column({comment:'资源名称'})  // 唯一
@@ -12,16 +18,19 @@ export class tk_authority extends tk_common{
     @Column({unique:true,comment:'资源唯一标记'})  // 唯一
     code:string
 
-    @Column({comment:'资源地址',nullable:true,})  
-    url:string
+    @Column({comment:'资源内容',default:'',nullable:false})  
+    content:string
 
     @Column({type:'int',unique:false,comment:'资源类型：1是菜单(与前端关联),2是api接口(与后端关联),3页面路由'})
-    auth_type:number
+    resource_type:number
 
-    @Column({comment:'上级id',default:0,nullable:true})  
+    @Column({default:null,comment:'资源所属应用'})
+    application:string
+
+    @Column({comment:'上级id',default:null})  
     pid:number
 
 
-    // @ManyToMany(() => tk_role, role => role.auths, { cascade: true })
-    // roles: tk_role[];
+    @ManyToMany(() => tk_role, role => role.resources, { cascade: true })
+    roles: tk_role[];
 }
--- src/entity/tk_role.entity.ts
+++ src/entity/tk_role.entity.ts
@@ -2,6 +2,7 @@ import { tk_authority } from './tk_authority.entity';
 import { tk_user } from '@/entity/tk_user.entity';
 import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { tk_common } from './tk_common';
+import { tk_resource } from './tk_resource.entity';
 // import { tk_user_role } from './tk_user_role.entity';
 /**
  * 角色实体
@@ -18,21 +19,24 @@ export class tk_role extends tk_common{
     @Column({comment:'角色类型1:系统预设,2:普通角色',default:2})  //
     role_type:number
     
-    // @Column({comment:'角色的父级id (所属)',nullable:true})  // 唯一
-    // pid:number
-
-  
-
-    // @OneToMany(type => tk_user_role, userRole => userRole.users)
-    // roleUsers: tk_user_role[];
+   
     @ManyToMany(type => tk_user, user => user.roles)
     users: tk_user[];
 
-    @ManyToMany(type => tk_authority, auth => auth.roles)
+    // @ManyToMany(type => tk_authority, auth => auth.roles)
+    // @JoinTable({
+    //     name: 'tk_role_authority',
+    //     joinColumns: [  {name: 'r_id'} ],
+    //     inverseJoinColumns: [ {name: 'a_id'}]
+    // })
+    // auths: tk_authority[]
+
+
+    @ManyToMany(type => tk_resource, resources => resources.roles)
     @JoinTable({
-        name: 'tk_role_authority',
-        joinColumns: [  {name: 'r_id'} ],
-        inverseJoinColumns: [ {name: 'a_id'}]
+        name: 'tk_role_resources',
+        joinColumns: [  {name: 'role_id'} ],
+        inverseJoinColumns: [ {name: 'resources_id'}]
     })
-    auths: tk_authority[]
+    resources:tk_resource[]
 }
